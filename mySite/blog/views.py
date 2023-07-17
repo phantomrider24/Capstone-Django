acd35fcb6b86bb18e455b241bbd29da6812e8c68
Onershen <onie.naidoo20@gmail.com>
2023-07-17 17:09:58 +0200

    Documentation added for mySite/blog/views.py

--- mySite/blog/views.py
+++ mySite/blog/views.py
@@ -9,12 +9,24 @@ from django.urls import reverse
 
 
 class Home_View(ListView):
+    """This class is a form from the django generic forms which Render some list of objects
+
+        :param model: is the allies that saves the Posts  
+        :param template_name: loads the template which is gonna be used for this function  
+    """
     model = Post
     template_name = 'home_page.html'
 
 def About_Me_View(request):
+    """About_Me_View function is a render function to my personal page with information about the developer
+    """
     return render(request, 'about_me.html',{})
 
 class Blog_details_View(DetailView):
+    """This class represent the blog details and posts the details to the post.html file
+
+        :param model: is the allies that saves the Posts  
+        :param template_name: loads the template which is gonna be used for this function 
+    """
     model = Post
     template_name = 'Post.html'
